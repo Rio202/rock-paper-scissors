825ce28dc3a2fbf8fc3599da4b86a22484cb8b90
Oriane <mizero.nsen@gmail.com>
2025-08-17 13:27:24 +0200

    Added comments to script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+//Declared the computers random choices to return a sting.
 
 function getComputerChoice(){
     let randomInt = Math.floor(Math.random()* 3);
@@ -14,16 +14,18 @@ function getComputerChoice(){
 }
 
 //console.log(getComputerChoice());
-
+//created a prompt to get the human choice
 function getHumanChoice(){
     return prompt("Rock, paper, scissors?");
 }
 
 //console.log(getHumanChoice());
+//Added the starting scores of the game.
 
 let humanScore = 0;
 let computerScore = 0;
 
+//Created the function for the game that compares the computer and human choice, and gives scores based on choices.
 
 function playRound(humanChoice, computerChoice){
     console.log(computerChoice);
@@ -50,7 +52,7 @@ function playRound(humanChoice, computerChoice){
         return ("rock beats scissors, you win");
     }
 }
-
+//Created function for 5 rounds and to call the score at the end.
 function playGame(){
     //let round = playRound(getHumanChoice().toLowerCase(), getComputerChoice())
 console.log(playRound(getHumanChoice().toLowerCase(), getComputerChoice()));
