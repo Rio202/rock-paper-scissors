7956b610f81cc4506d464d2e7332dc4da81e93c6
Oriane <95043409+Rio202@users.noreply.github.com>
2026-07-27 11:48:19 +0200

    Added buttons, outcomes and results to HTML file and UI to Javascript file

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+//Added the starting scores of the game.
+let playerScore = 0;
+let computerScore = 0;
+
+const rockButton = document.querySelector(".rock")
+const paperButton = document.querySelector(".paper")
+const scissorsButton = document.querySelector(".scissors")
+const outcomeDiv = document.querySelector(".outcome")
+const playerSpan = document.querySelector(".player-score")
+const compSpan = document.querySelector(".computer-score")
 
 //Declared the computers random choices to return a sting.
-
 function getComputerChoice(){
     let randomInt = Math.floor(Math.random()* 3);
     if (randomInt == 0){
@@ -13,57 +22,109 @@ function getComputerChoice(){
     
 }
 
-//console.log(getComputerChoice());
 //created a prompt to get the human choice
-function getHumanChoice(){
-    return prompt("Rock, paper, scissors?");
-}
+    //function getHumanChoice(){
+    //return prompt("Rock, paper, scissors?");
+    //}
 
 //console.log(getHumanChoice());
-//Added the starting scores of the game.
 
-let humanScore = 0;
-let computerScore = 0;
+
+
 
 //Created the function for the game that compares the computer and human choice, and gives scores based on choices.
 
-function playRound(humanChoice, computerChoice){
-    console.log(computerChoice);
-    console.log(humanChoice);
-    if (humanChoice === computerChoice){
-       return "it's a draw";
-    } else if (humanChoice == "rock" && computerChoice == "paper"){
-        computerScore ++ ;
-        return ("paper beats rock, you lose");
-    } else if (humanChoice == "paper" && computerChoice == "rock"){
-        humanScore ++ ;
-        return ("paper beats rock, you win");
-    } else if (humanChoice == "scissors" && computerChoice == "paper"){
-        computerScore ++ ;
-        return ("scissors beats paper, you win");
-    } else if (humanChoice == "paper" && computerChoice == "scissors"){
-        computerScore ++ ;
-        return ("scissors beats paper, you lose");
-    } else if (humanChoice == "scissors" && computerChoice == "rock"){
-        computerScore ++ ;
-        return ("rock beats scissors, you lose");
-    } else if (humanChoice == "rock" && computerChoice == "scissors"){
-        humanScore ++ ;
-        return ("rock beats scissors, you win");
+function playRound(playerSelection, computerChoice){
+    
+    if (playerSelection === computerChoice){
+//creates p element, adds text to p element, and adds the p element to DOM
+       const p = document.createElement("p")
+       p.innerText = "it's a draw";
+       outcomeDiv.appendChild(p)
+    } else if (playerSelection == "rock" && computerChoice == "paper"){
+        computerScore ++ 
+        const p = document.createElement("p") 
+        p.innerText = "paper beats rock, you lose";
+        outcomeDiv.appendChild(p)
+    } else if (playerSelection == "paper" && computerChoice == "rock"){
+        playerScore ++ ;
+        const p = document.createElement("p") 
+        p.innerText = "paper beats rock, you win";
+        outcomeDiv.appendChild(p)
+    } else if (playerSelection== "scissors" && computerChoice == "paper"){
+        computerScore ++ 
+        const p = document.createElement("p") 
+        p.innerText = "scissors beats paper, you win";
+        outcomeDiv.appendChild(p)
+    } else if (playerSelection == "paper" && computerChoice == "scissors"){
+        computerScore ++ 
+        const p = document.createElement("p") 
+        p.innerText = "scissors beats paper, you lose";
+        outcomeDiv.appendChild(p)
+    } else if (playerSelection == "scissors" && computerChoice == "rock"){
+        computerScore ++ 
+        const p = document.createElement("p") 
+        p.innerText = "rock beats scissors, you lose";
+        outcomeDiv.appendChild(p)
+    } else if (playerSelection == "rock" && computerChoice == "scissors"){
+        playerScore ++ 
+        const p = document.createElement("p") 
+        p.innerText = "rock beats scissors, you win";
+        outcomeDiv.appendChild(p)
     }
 }
-//Created function for 5 rounds and to call the score at the end.
-function playGame(){
-    //let round = playRound(getHumanChoice().toLowerCase(), getComputerChoice())
-console.log(playRound(getHumanChoice().toLowerCase(), getComputerChoice()));
-console.log(playRound(getHumanChoice().toLowerCase(), getComputerChoice()));
-console.log(playRound(getHumanChoice().toLowerCase(), getComputerChoice()));
-console.log(playRound(getHumanChoice().toLowerCase(), getComputerChoice()));
-console.log(playRound(getHumanChoice().toLowerCase(), getComputerChoice()));
 
-console.log(humanScore);
-console.log(computerScore);
-
+const whoWon = (playerScore, computerScore) => {
+    if (playerScore === 5){
+        const h2 = document.createElement("h2")
+        h2.innerText = "YOU WON!"
+        outcomeDiv.appendChild(h2)
+    } else if (computerScore === 5){
+        const h2 = document.createElement("h2")
+        h2.innerText = "YOU LOST!"
+        outcomeDiv.appendChild(h2)
+    }
+    
+}
+//Adding the scores to the page
+const updateScores = (playerScore, computerScore) =>{
+    playerSpan.innerText = `${playerScore}`
+    compSpan.innerText = `${computerScore}`
 }
 
-playGame();
+//add eventListener to buttons
+rockButton.addEventListener("click", () => {
+    const computerChoice = getComputerChoice()
+    const playerSelection = "rock"
+    playRound(playerSelection,computerChoice)
+    updateScores(playerScore, computerScore)
+    whoWon(playerScore, computerScore)
+})
+paperButton.addEventListener("click", () => {
+    const computerChoice = getComputerChoice()
+    const playerSelection = "paper"
+    playRound(playerSelection,computerChoice)
+    updateScores(playerScore, computerScore)
+    whoWon(playerScore, computerScore)
+})
+scissorsButton.addEventListener("click", () => {
+    const computerChoice = getComputerChoice()
+    const playerSelection = "scissors"
+    playRound(playerSelection,computerChoice)
+    updateScores(playerScore, computerScore)
+    whoWon(playerScore, computerScore)
+})
+
+
+
+//Created function for 5 rounds and to call the score at the end.
+//const playGame = () => {
+    //for(let i = 0; i < 5; i++)
+       // const playerSelection = prompt ("Rock, Paper, Scissors").toLowerCase
+       // const computerChoice = getComputerChoice
+//}
+
+
+
+//playGame();
+
